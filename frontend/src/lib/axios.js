0556035e83601f33180a402e8594f1003c0301c1
Konrad Patla <konradpatla@gmail.com>
2026-04-03 17:14:10 +0200

    prepered for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+//in production, there's no localhost so we have to make this dynamic
+//jest to właściwość vite, która sprawdza czy jesteśmy w production czy dev
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+//ustawiamy tutaj base url
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: BASE_URL,
 });
 
 export default api;
